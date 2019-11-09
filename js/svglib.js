78c2306e98710200f08a76e40125cee9b5b26ebc
Ahmed Sharaf <ahmedsharaf@aucegypt.edu>
2019-11-09 20:54:49 +0200

    Adding Feature f Dynamically adding SVG and hiding the to the canvas

--- js/svglib.js
+++ js/svglib.js
@@ -16,22 +16,54 @@ var options = {
 var svgElement = document.querySelector('#svg-container');
 var panZoomTiger = svgPanZoom(svgElement,options);
 
-var num = 1;
+var num = 0;
 function layer(num) {
     this.index = num;
     this.add = function(){
         console.log(this.index);
     };
-  }
-function createLayer(){
-    var l = new layer();
-    l.add();
-    return l;
 }
-function getIndex(){
-    num = num + 1;
-    return num;
+function makeSVGEl(tag, attrs) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs) {
+      el.setAttribute(k, attrs[k]);
+    }
+    return el;
+}
+function createLayer(){
+    var option = "<input type='checkbox'  class='opt_layer' data-num=" + ++num + " checked>Layer "+ num +"<br>";
+    $(".sidenav").append(option);
+    var html = makeSVGEl("g", {id: "group_" + num });
+    $("#svg-container").append(html);
 }
 
-var l1 = createLayer(getIndex());
-var l2 = createLayer(getIndex());
+function createCell(x, y, h, w, color){
+
+}
+
+function createMetal(_x, _y, _h, _w){
+    var html = makeSVGEl("rect", { x: _x, y: _y, height: _h, width: _w, fill:"url(#hashed_1)" });
+    $("#group_" + num).append(html);
+}
+
+$(document).ready(function() {
+    //set initial state.
+    $('.opt_layer').change(function() {
+        var n = $(this).data('num');
+        if(this.checked) {
+            $("#group_" + n).show();
+        } else {
+            $("#group_" + n).hide();
+        }
+        $('#textbox1').val(this.checked);        
+    });
+});
+
+createLayer();
+createMetal(0,0,50,50);
+createMetal(20,20,50,50);
+createMetal(40,40,50,50);
+createLayer();
+createMetal(60,60,50,50);
+createMetal(80,80,50,50);
+createMetal(100,100,10,10);
