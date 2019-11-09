20566a94f6b78a6ecef13acc69c8054e80c9bd40
Ahmed Sharaf <ahmedsharaf@aucegypt.edu>
2019-11-10 01:09:36 +0200

    Adding Wires layers to the svg

--- js/svglib.js
+++ js/svglib.js
@@ -30,19 +30,39 @@ function makeSVGEl(tag, attrs) {
     }
     return el;
 }
-function createLayer(){
-    var option = "<input type='checkbox'  class='opt_layer' data-num=" + ++num + " checked>Layer "+ num +"<br>";
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function createLayer(r,b,g){
+    //RBGA Accepts values between 0 and 255
+    var r = getRandomInt(256);
+    var b = getRandomInt(256);
+    var g = 0; //Removing the green component so that the line gets contast with the background
+    var option = "<input type='checkbox' class='opt_layer' data-num=" + ++num + " checked>Layer "+ num +"<br>";
     $(".sidenav").append(option);
-    var html = makeSVGEl("g", {id: "group_" + num });
+    var html = makeSVGEl("g", {id: "group_" + num, "data-r":r,"data-b":b,"data-g":g });
     $(".svg-pan-zoom_viewport").append(html);
 }
 
-function createCell(x, y, h, w, color){
-
+function createCell(_x, _y, _h, _w, r, b, g){
+    var html = makeSVGEl("rect",
+     { x: _x, y: _y, height: _h, width: _w, fill:"rgba("+r+","+g+","+b+", 0.3)", style:"stroke:rgba("+r+","+g+","+b+", 1);stroke-width:0.5"});
+    $("#group_" + num).append(html);
 }
 
-function createMetal(_x, _y, _h, _w){
-    var html = makeSVGEl("rect", { x: _x, y: _y, height: _h, width: _w, fill:"url(#hashed_1)" });
+function createWire(p1x, p1y, p2x, p2y){
+    var r = $("#group_" + num).data('r');
+    var g = $("#group_" + num).data('g');
+    var b = $("#group_" + num).data('b');
+    var html = makeSVGEl("line",
+     { x1: p1x, y1: p1y, x2: p2x, y2: p2y,  style:"stroke:rgb("+r+","+g+","+b+");stroke-width:2"});
+    $("#group_" + num).append(html);
+}
+
+function createFF(_x, _y, _h, _w){
+    var html = makeSVGEl("rect",
+     { x: _x, y: _y, height: _h, width: _w, fill:"url(#hashed_1)", style:"stroke:color(rgb(0%,0%,100%););stroke-width:0.5"});
     $("#group_" + num).append(html);
 }
 
@@ -59,11 +79,7 @@ $(document).ready(function() {
     });
 });
 
-createLayer();
-createMetal(0,0,50,50);
-createMetal(20,20,50,50);
-createMetal(40,40,50,50);
-createLayer();
-createMetal(60,60,50,50);
-createMetal(80,80,50,50);
-createMetal(100,100,10,10);
+createLayer(0,0,0);
+createWire(0,0,100,50);
+createWire(20,20,50,50);
+createWire(40,40,50,50);
