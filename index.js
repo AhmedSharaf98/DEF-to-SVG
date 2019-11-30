273f2bd2c3e168dd3cecb89829568f4636d6be80
Alaa <alaa44010@aucegypt.edu>
2019-11-30 22:31:48 +0200

    drc added

--- index.js
+++ index.js
@@ -5,7 +5,84 @@ $(document).ready(function(){
 var lefLoaded = false;
 defInput = document.getElementById('defs');
 lefInput = document.getElementById('lefs');
-viewbtn = document.getElementById('viewbtn')
+viewbtn = document.getElementById('viewbtn');
+
+/************************************************/
+/**********************DRC***********************/
+spacing = {1:200, 2:160, 3:200, 4:320}  // from the lef file
+all_wires = {};
+all_vias = {};
+all_violations = [];
+function record(coord, metal_layer){
+    if (coord.length == 1){
+        if (all_vias[metal_layer] == null)
+            all_vias[metal_layer] = []
+        all_vias[metal_layer].push(coord);
+    }
+    else if (coord.length == 2){
+        if (all_wires[metal_layer] == null)
+            all_wires[metal_layer] = {h:[], v:[]};
+        if(coord[0].y == coord[1].y)    //horizontal    
+            all_wires[metal_layer].h.push(coord);
+        else if (coord[0].x == coord[1].x)    //vertical    
+            all_wires[metal_layer].v.push(coord);
+        else
+            console.log("invalid wire!!");
+    }
+}
+function sort_all_records(){
+    for (key in Object.keys(all_wires)){
+        if (all_wires.hasOwnProperty(key)){
+            all_wires[key].h.sort(function (a,b){
+                return (a[0].y < b[0].y)? -1 : 1; 
+            });
+            all_wires[key].v.sort(function (a,b){
+                return (a[0].x < b[0].x)? -1 : 1; 
+            });
+        }
+    }
+}
+function intersect_vertically(a, b){
+    return !((Math.min(a[0].x, a[1].x) >= Math.max(b[0].x, b[1].x)) || (Math.min(b[0].x, b[1].x) >= Math.max(a[0].x, a[1].x)));
+}
+function intersect_horizontally(a, b){
+    return !((Math.min(a[0].y, a[1].y) >= Math.max(b[0].y, b[1].y)) || (Math.min(b[0].y, b[1].y) >= Math.max(a[0].y, a[1].y)));
+}
+function vertical_spacing_violation(a, b, metal_layer){
+    return (Math.abs(a[0].y - b[0].y) < spacing[metal_layer]);
+}
+function horizontal_spacing_violation(a, b, metal_layer){
+    return (Math.abs(a[0].x - b[0].x) < spacing[metal_layer]);
+}
+function check_drc(){
+    for (key in Object.keys(all_wires)){
+        if (all_wires.hasOwnProperty(key)){
+            m = all_wires[key];
+            for (i=0;i<m.h.length-1; i++){
+                for (j=i+1;j<m.h.length;j++){
+                    if (!vertical_spacing_violation(m.h[i], m.h[j], key))
+                        break;
+                    if (intersect_vertically(m.h[i], m.h[j])){
+                        console.log("violation!");
+                        all_violations.push([m.h[i], m.h[j]]);
+                    }
+                }
+            }
+            for (i=0;i<m.v.length-1; i++){
+                for (j=i+1;j<m.v.length;j++){
+                    if (!horizontal_spacing_violation(m.v[i], m.v[j], key))
+                        break;
+                    if (intersect_horizontally(m.v[i], m.v[j])){
+                        console.log("violation!");
+                        all_violations.push([m.v[i], m.v[j]]);
+                    }
+                }
+            }
+        }
+    }
+}
+/************************************************/
+
 if(defInput){
 defInput.addEventListener("change", function(Event){
     var files = event.target.files;
@@ -124,8 +201,17 @@ viewbtn.addEventListener("click", function(Event){
                     createClkNet(metal_layer, getLayerWidth(metal_layer), x1, y1, x2, y2);
                 else
                     createNet(metal_layer, getLayerWidth(metal_layer), x1, y1, x2, y2);
+
+                //DRC
+                record(coord, metal_layer);
             }
         }
+        sort_all_records();
+        console.log(all_wires);
+        console.log(all_vias);
+        check_drc();
+        console.log(all_violations);
+
         var div = document.getElementById('sidnavRight');
         for (var key in celltypeToColor)
             div.innerHTML += "<h6 style=background-color:rgb("+celltypeToColor[key].r+','+celltypeToColor[key].g+','+celltypeToColor[key].b+")>"+key+"</h6>"; ;
